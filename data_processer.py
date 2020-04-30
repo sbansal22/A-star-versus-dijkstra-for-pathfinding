a4014d6ed264092fe344de342661fe2fdf5c8e7e
hkRho <hrho@olin.edu>
2020-04-30 02:33:39 -0400

    Change hashing and fix issues

--- data_processer.py
+++ data_processer.py
@@ -3,107 +3,89 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from geopy.geocoders import Nominatim
 
-# def Euclidean(x, y):
-#     geolocator = Nominatim()
-#     x_com = x + ', Boston, Massachusetts'
-#     y_com = y + ', Boston, Massachusetts'
-#     x_loc = geolocator.geocode(x_com)
-#     y_loc = geolocator.geocode(y_com)
-#     if x_loc is None or y_loc is None:
-#         return print(x_com, y_com)
-#     return (((x_loc.latitude-y_loc.latitude)**2 + ((x_loc.longitude-y_loc.longitude)**2)))
+# # def Euclidean(x, y):
+# #     geolocator = Nominatim()
+# #     x_com = x + ', Boston, Massachusetts'
+# #     y_com = y + ', Boston, Massachusetts'
+# #     x_loc = geolocator.geocode(x_com)
+# #     y_loc = geolocator.geocode(y_com)
+# #     if x_loc is None or y_loc is None:
+# #         return print(x_com, y_com)
+# #     return (((x_loc.latitude-y_loc.latitude)**2 + ((x_loc.longitude-y_loc.longitude)**2)))
 
 def geo(G, street_hashmap):
     long_lat = dict()
     geolocator = Nominatim(user_agent="google maps")
     # List of encoded nodes
     node_list = list(G.nodes())
-    print(node_list)
     node_list_word = []
     for i in node_list:
         node_list_word.append(street_hashmap[i])
     for street in node_list:
-        street_com = street_hashmap[street_hashmap((node_list[street]))] + ', Boston, Massachusetts'
+        street_com = street_hashmap[street] + ', Boston, Massachusetts'
         street_loc = geolocator.geocode(street_com)
-        if street_com is None:
+        if street_loc is None:
             continue
         else:
-            long_lat[street] = (street_loc.latitude, street_loc.long_longitude)
+            long_lat[street] = (street_loc.latitude, street_loc.longitude)
     
     return long_lat
 
+
+def drop_dead_ends(df):
+    df.drop(df.loc[df['to']=='Dead End'].index, inplace=True)
+    # df.reset_index(drop=True)
+
+
 def data_to_graph():
     data = pd.read_excel(r'St-Data-Original.xlsx')
-    # print(data)
-
     df = pd.DataFrame(data)
-    st_name = df['St_name']
-    from_st = df['from']
-    to_st = df['to']
-    miles = df['miles']
 
+    # find rows that contain 'Dead End' and delete them from df
+    drop_dead_ends(df)
+    from_st = df['from'].values.tolist()
+    to_st = df['to'].values.tolist()
+    miles = df['miles'].values.tolist()
 
-    location_map = dict()
+    street_hashmap = dict()
     G = nx.Graph()
 
-
     for i in range(len(df.index)):
-
         # if the dictionary is empty
-        if len(location_map) == 0:
-            if to_st[i] == "Dead End":
-                location_map[st_name[i]] = i
-                G.add_node(location_map[st_name[i]])
-                location_map[from_st[i]] = i+1
-                G.add_node(location_map[from_st[i]])
-                # print("A")
-                G.add_edge(location_map[st_name[i]], location_map[from_st[i]], weight=miles[i])
-            
-            else:
-                location_map[from_st[i]] = i
-                G.add_node(location_map[from_st[i]])
-                location_map[to_st[i]] = i+1
-                G.add_node(location_map[to_st[i]])
-                # print("B")
-                G.add_edge(location_map[from_st[i]], location_map[to_st[i]], weight=miles[i])
-
+        if len(street_hashmap) == 0:
+            street_hashmap[i] = from_st[i]
+            G.add_node(i)
+            street_hashmap[i+1] = to_st[i]
+            G.add_node(i+1)
+            G.add_edge(i, i+1, weight=miles[i])
+        
         # if the dictionary is not empty 
         else:
-            # if the to_st has a dead end, just make an edge between the st name and from
-            if to_st[i] == "Dead End":
-                if st_name[i] not in location_map:
-                    location_map[st_name[i]] = max(location_map.values()) + 1
-                    G.add_node(location_map[st_name[i]])
+            if from_st[i] not in street_hashmap.values():
+                street_hashmap[max(street_hashmap.keys()) + 1] = from_st[i]
+                node = list(street_hashmap.keys())[list(street_hashmap.values()).index(from_st[i])]
+                G.add_node(node)
 
-                if from_st[i] not in location_map:
-                    location_map[from_st[i]] = max(location_map.values()) + 1
-                    G.add_node(location_map[from_st[i]])
+            if to_st[i] not in street_hashmap.values():
+                street_hashmap[max(street_hashmap.keys()) + 1] = to_st[i]
+                node = list(street_hashmap.keys())[list(street_hashmap.values()).index(to_st[i])]
+                G.add_node(node)
 
-                # print("C")
-                G.add_edge(location_map[st_name[i]], location_map[from_st[i]], weight=miles[i])
-
-            # if no dead end, make edge between from_st and to_st
-            else:
-                if from_st[i] not in location_map:
-                    location_map[from_st[i]] = max(location_map.values()) + 1
-                    G.add_node(location_map[from_st[i]])
-
-                if to_st[i] not in location_map:
-                    location_map[to_st[i]] = max(location_map.values()) + 1
-                    G.add_node(location_map[to_st[i]])
-
-                # print(location_map[from_st[i]])
-                # print("D")
-                G.add_edge(location_map[from_st[i]], location_map[to_st[i]], weight=miles[i])
+            from_st_node = list(street_hashmap.keys())[list(street_hashmap.values()).index(from_st[i])]
+            to_st_node = list(street_hashmap.keys())[list(street_hashmap.values()).index(to_st[i])]
+            G.add_edge(from_st_node, to_st_node, weight=miles[i])
     
-    return G, location_map
+    return G, street_hashmap
 
-
+        
 if __name__ == "__main__":
+    ### TEST data_to_graph() ###
     G, street_hashmap = data_to_graph()
-    print(street_hashmap)
+    # print(street_hashmap)
     # nx.draw(G, with_labels=True, font_weight='bold')
     # plt.show()
+
+    ### TEST geo() ###
     long_lat = geo(G, street_hashmap)
-    # print(long_lat['Washington St.'])
+    # # print(long_lat['Washington St.'])
     print(long_lat)
